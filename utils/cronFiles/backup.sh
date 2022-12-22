c64f9dc1251b214c386f28fd038ced3ce20faa0e
Ariel <mileguir23@hotmail.com>
2022-12-21 23:58:35 -0300

    Versão com o output da data (teste do cron) funcinando.

--- utils/cronFiles/backup.sh
+++ utils/cronFiles/backup.sh
@@ -1,3 +1,2 @@
 data=$(/bin/date +\%d-\%m-\%Y--\%H:\%M:\%S)
-
-pg_dump -h pokemarket_database -p 5432 -U postgres -w -Fc -f backup/$data.dump
+pg_dump -h pokemarket_database -p 5432 -U postgres -w -Fc >> $data.dump
